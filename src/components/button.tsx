6b82cff71676c811f6d374a48c730a64e0ac3866
mrnlldn <aldabamarinelladiane@gmail.com>
2023-02-11 17:48:10 +0800

    allow extending of default styling for custom button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,10 +5,10 @@ type ButtonProps = {
   variant?: 'primary' | 'secondary'
 } & ButtonHTMLAttributes<HTMLButtonElement>
 
-const Button = ({ variant = 'primary', ...props }: ButtonProps) => {
+const Button = ({ variant = 'primary', className, ...props }: ButtonProps) => {
   return (
     <button
-      className={clsx('p-2 rounded-md', {
+      className={clsx('p-2 rounded-md', className, {
         'bg-blue-500 hover:bg-blue-600 text-white': variant === 'primary',
         'bg-gray-500 hover:bg-gray-600 text-white': variant === 'secondary',
       })}
