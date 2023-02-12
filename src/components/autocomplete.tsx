5af2ba1015688a77a2a88681a9eda1543b612723
mrnlldn <aldabamarinelladiane@gmail.com>
2023-02-12 08:19:41 +0800

    improve styling of autocomplete component

--- src/components/autocomplete.tsx
+++ src/components/autocomplete.tsx
@@ -1,37 +1,50 @@
 import { Combobox, Transition } from '@headlessui/react'
+import { type ReactNode, Fragment } from 'react'
 
-type AutocompleteItem<T> = {
-  label: string
-  value: T
-}
 type AutocompleteProps<T> = {
   value: T | null
-  options: AutocompleteItem<T>[]
+  displayValue: (value: T | null) => string
+  options: T[]
   onInputChange: (newValue: string) => void
   onChange: (newValue: T) => void
+  renderOption: (option: T) => ReactNode
 }
 
 const Autocomplete = <T,>({
   value,
+  displayValue,
   options,
   onInputChange,
   onChange,
+  renderOption,
 }: AutocompleteProps<T>) => {
   return (
     <Combobox value={value} onChange={onChange}>
       <Combobox.Input
+        displayValue={displayValue}
         onChange={(e) => onInputChange(e.target.value)}
-        className="w-full rounded-md py-2 text-sm leading-5 text-gray-900"
+        className="w-full rounded-md py-2 text-sm leading-5 text-gray-900 px-2 border-2 border-gray-600"
       />
-      <Combobox.Options>
-        {options.map((option, idx) => {
-          return (
-            <Combobox.Option key={idx} value={option.value}>
-              {option.label}
-            </Combobox.Option>
-          )
-        })}
-      </Combobox.Options>
+      <Transition
+        as={Fragment}
+        leave="transition ease-in duration-100"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <Combobox.Options className="border-2 mt-4 border-gray-600 rounded-md z-20 absolute max-h-60 overflow-auto bg-white">
+          {options.map((option, idx) => {
+            return (
+              <Combobox.Option
+                key={idx}
+                value={option}
+                className="relative cursor-default select-none"
+              >
+                {renderOption(option)}
+              </Combobox.Option>
+            )
+          })}
+        </Combobox.Options>
+      </Transition>
     </Combobox>
   )
 }
--- src/features/home/screens/home.screen.tsx
+++ src/features/home/screens/home.screen.tsx
@@ -7,9 +7,29 @@
 import Autocomplete from '@/components/autocomplete'
 import Button from '@/components/button'
 import { useActiveUser } from '@/components/hooks/useActiveUser'
+import clsx from 'clsx'
+import { useState } from 'react'
+import { optional } from 'zod'
+
+const options = [
+  {
+    name: 'London',
+    lat: 123,
+    lon: 456,
+  },
+  {
+    name: 'Britain',
+    lat: 789,
+    lon: 654,
+  },
+]
 
 const HomeScreen = () => {
   const user = useActiveUser()
+
+  const [q, setQ] = useState('')
+  const [selected, setSelected] = useState<typeof options[0] | null>(null)
+
   return (
     <section className="items-center flex flex-col space-y-16">
       <div className="space-y-2 font-semibold text-center hidden md:block">
@@ -18,13 +38,30 @@ const HomeScreen = () => {
       </div>
       <div className="space-y-2 flex flex-col content-center items-center">
         <Autocomplete
-          value={null}
-          options={[]}
-          onChange={() => {
-            return
+          value={selected}
+          displayValue={(option) => option?.name ?? ''}
+          options={options}
+          renderOption={(option) => {
+            return (
+              <div
+                className={clsx(
+                  'w-full bg-white hover:bg-gray-600 hover:text-white py-2 pl-5 pr-4',
+                  {
+                    'bg-blue-600 hover:bg-blue-700':
+                      option.lat === selected?.lat &&
+                      option.lon === selected?.lon,
+                  },
+                )}
+              >
+                <p>{`${option.name} (${option.lat}, ${option.lon})`}</p>
+              </div>
+            )
           }}
-          onInputChange={() => {
-            return
+          onChange={(newValue) => {
+            setSelected(newValue)
+          }}
+          onInputChange={(newInput) => {
+            setQ(newInput)
           }}
         />
         <Button>Display Weather</Button>
